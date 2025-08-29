ac2d78bcd516a5385b2e20adcddf26a28bb15e15
Diyorbek-008 <diyorbeksunnatillayev09@gmail.com>
2025-08-29 12:54:01 +0500

    final

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 const loginPage = () => {
-    window.location.href = "./login.html"
-
-    
+    window.location.href = "./login.html"   
 }
+
+
+let cards = document.getElementById("links")
+
+function chiqar(data) {
+    cards.innerHTML = null
+
+    data.forEach((item) => {
+        const li = document.createElement("li")
+        li.className = "link"
+
+        li.innerHTML = `
+        <img class="book-img" src=${item.rasm} width="300px" height="400px"/>
+
+                        <div class="book-info">
+                            <h2 class="book-type">${item.KitobNomi}</h2>
+                            <p class="author">${item.Yozuvchisi}</p>
+                            <p class="year">${item.Yili}</p>
+
+
+
+                            <div class="buttons">
+                                <button class="btn">Bookmark</button>
+                                <button class="btn2">Info</button>
+                            </div>
+                            <button class="btn3" onclick="">Read</button>
+                        </div>
+        `
+
+        cards.appendChild(li)
+    })
+}
+
+chiqar(data)
